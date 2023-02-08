6f4178e0937ee26fc2a48b78cdc37d31c9885a19
Dima Savkov <dimsaw85@gmail.com>
2023-02-08 17:26:58 +0200

    add router

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const uniqid = require("uniqid");
 
-router.get("/", (req, res) => {});
+const posts = [
+  { id: "1", topic: "test1", text: "test text1" },
+  { id: "2", topic: "test2", text: "test text2" },
+  { id: "3", topic: "test3", text: "test text3" },
+  { id: "4", topic: "test4", text: "test text4" },
+  { id: "5", topic: "test5", text: "test text5" },
+  { id: "6", topic: "test6", text: "test text6" },
+];
 
-router.get("/:id", (req, res) => {});
+router.get("/", (req, res) => {
+  res.json({ posts, status: "success" });
+});
+
+router.get("/:id", (req, res) => {
+  const [post] = posts.filter((item) => item.id === req.params.id);
+  res.json({ post, status: "success" });
+});
+
+router.post("/", (req, res) => {
+  const { topic, text } = req.body;
+  posts.push({
+    id: uniqid(),
+    topic,
+    text,
+  });
+  res.json({ status: "success" });
+});
+
+// router.push("/:id", (req, res) => {});
+
+// router.delete("/:id", (req, res) => {});
 
 module.exports = { postsRouter: router };
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json());
+app.use(morgan("tiny"));
+
 const PORT = process.env.PORT || 8081;
 
 const { postsRouter } = require("./routers/postsRouter.js");
-
-app.use(express.json());
-app.use(morgan("tiny"));
 
 app.use("/api/posts", postsRouter);
 
