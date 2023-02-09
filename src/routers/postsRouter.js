ca119a201cd595b2c354cef04a449db815872fc1
Dima Savkov <dimsaw85@gmail.com>
2023-02-09 10:36:37 +0200

    add controllers

--- src/routers/postsRouter.js
+++ src/routers/postsRouter.js
@@ -1,77 +1,31 @@
 const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
-const uniqid = require("uniqid");
+
 const {
   addPostValidation,
   pathPostValidation,
 } = require("../middlewares/validationMiddleware");
 
-let posts = [
-  { id: "1", topic: "test1", text: "test text1" },
-  { id: "2", topic: "test2", text: "test text2" },
-  { id: "3", topic: "test3", text: "test text3" },
-  { id: "4", topic: "test4", text: "test text4" },
-  { id: "5", topic: "test5", text: "test text5" },
-  { id: "6", topic: "test6", text: "test text6" },
-];
+const {
+  getPost,
+  getPostById,
+  addPost,
+  changePost,
+  patchPost,
+  deletePost,
+} = require("../controllers/controllers");
 
-router.get("/", (req, res) => {
-  res.json({ posts, status: "success" });
-});
+router.get("/", getPost);
 
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const [post] = posts.filter((item) => item.id === id);
+router.get("/:id", getPostById);
 
-  if (!post) {
-    return res
-      .status(400)
-      .json({ status: `failure, no post with this id: ${id}` });
-  }
-  res.json({ post, status: "success" });
-});
+router.post("/", addPostValidation, addPost);
 
-router.post("/", addPostValidation, (req, res) => {
-  const { topic, text } = req.body;
-  posts.push({
-    id: uniqid(),
-    topic,
-    text,
-  });
-  res.json({ status: "success" });
-});
+router.put("/:id", addPostValidation, changePost);
 
-router.put("/:id", addPostValidation, (req, res) => {
-  const { topic, text } = req.body;
+router.patch("/:id", pathPostValidation, patchPost);
 
-  posts.forEach((post) => {
-    if (post.id === req.params.id) {
-      post.topic = topic;
-      post.text = text;
-    }
-  });
-  res.json({ status: "success" });
-});
-
-router.patch("/:id", pathPostValidation, (req, res) => {
-  const { topic, text } = req.body;
-  posts.forEach((post) => {
-    if (post.id === req.params.id) {
-      if (topic) {
-        post.topic = topic;
-      }
-      if (text) {
-        post.text = text;
-      }
-    }
-  });
-  res.json({ status: "success" });
-});
-
-router.delete("/:id", (req, res) => {
-  posts = posts.filter((item) => item.id !== req.params.id);
-  res.json({ status: "success" });
-});
+router.delete("/:id", deletePost);
 
 module.exports = { postsRouter: router };
