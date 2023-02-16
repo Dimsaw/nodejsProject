faff6f32df233f1eb18dc0307b2020f655728e30
Dima Savkov <dimsaw85@gmail.com>
2023-02-16 12:58:22 +0200

    search problem

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -43,3 +43,49 @@ module.exports = {
   changePostController,
   deletePostController,
 };
+
+// const {
+//   getPosts,
+//   getPostsById,
+//   addPost,
+//   changePostById,
+//   deletePostById,
+// } = require("../services/postsService");
+
+// const getPostsController = async (req, res) => {
+//   const posts = await getPosts();
+//   res.json({ posts });
+// };
+
+// const getPostByIdController = async (req, res) => {
+//   const { id } = req.params;
+//   const post = await getPostsById(id);
+//   res.json({ post, status: "success" });
+// };
+
+// const addPostController = async (req, res) => {
+//   const { topic, text } = req.body;
+//   await addPost({ topic, text });
+//   res.json({ status: "success" });
+// };
+
+// const changePostController = async (req, res) => {
+//   const { id } = req.params;
+//   const { topic, text } = req.body;
+//   await changePostById(id, { topic, text });
+//   res.json({ status: "success" });
+// };
+
+// const deletePostController = async (req, res) => {
+//   const { id } = req.params;
+//   await deletePostById(id);
+//   res.json({ status: "success" });
+// };
+
+// module.exports = {
+//   getPostsController,
+//   getPostByIdController,
+//   addPostController,
+//   changePostController,
+//   deletePostController,
+// };
