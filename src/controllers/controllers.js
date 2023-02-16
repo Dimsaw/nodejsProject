c8f0d3e6ea99fdffc1061f3b56b00f5623c5ba68
Dima Savkov <dimsaw85@gmail.com>
2023-02-16 13:00:21 +0200

    second try

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,47 +1,49 @@
-const {
-  getPosts,
-  getPostsById,
-  addPost,
-  changePostById,
-  deletePostById,
-} = require("../services/postsService");
+const ObjectId = require("mongodb").ObjectId;
 
-const getPostsController = async (req, res) => {
-  const posts = await getPosts();
+const getPost = async (req, res) => {
+  const posts = await req.db.Posts.find({}).toArray();
   res.json({ posts });
 };
 
-const getPostByIdController = async (req, res) => {
+const getPostById = async (req, res) => {
   const { id } = req.params;
-  const post = await getPostsById(id);
+  const post = await req.db.Posts.findOne({ _id: new ObjectId(id) });
+  if (!post) {
+    return res
+      .status(400)
+      .json({ status: `failure, no post with this id: ${id}` });
+  }
   res.json({ post, status: "success" });
 };
 
-const addPostController = async (req, res) => {
+const addPost = async (req, res) => {
   const { topic, text } = req.body;
-  await req.db.Posts.insertOne({ text, topic });
+  await addPost({ topic, text });
+  res.json({ post, status: "success" });
+};
+
+const changePost = async (req, res) => {
+  const { topic, text } = req.body;
+
+  await req.db.Posts.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: { topic, text } }
+  );
+
   res.json({ status: "success" });
 };
 
-const changePostController = async (req, res) => {
-  const { id } = req.params;
-  const { topic, text } = req.body;
-  await changePostById(id, { topic, text });
-  res.json({ status: "success" });
-};
-
-const deletePostController = async (req, res) => {
-  const { id } = req.params;
-  await deletePostById(id);
+const deletePost = async (req, res) => {
+  await req.db.Posts.deleteOne({ _id: new ObjectId(req.params.id) });
   res.json({ status: "success" });
 };
 
 module.exports = {
-  getPostsController,
-  getPostByIdController,
-  addPostController,
-  changePostController,
-  deletePostController,
+  getPost,
+  getPostById,
+  addPost,
+  changePost,
+  deletePost,
 };
 
 // const {
