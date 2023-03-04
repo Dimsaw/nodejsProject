6bc3dcf563828a2f6b6327e69d49c9d90e5e356b
Dima Savkov <dimsaw85@gmail.com>
2023-03-04 22:21:02 +0200

    work with google

--- google-storage-demo.js
+++ google-storage-demo.js
@@ -1,14 +1,30 @@
 const { Storage } = require("@google-cloud/storage");
 
 const storage = new Storage();
-const bucketName = test - nodejs - dima;
-const filePath = "./tmp/8777a8bb-b5b8-410b-833f-58b8542e8c79.jpg";
-const destFileName = "8777a8bb-b5b8-410b-833f-58b8542e8c79.jpg";
+const bucketName = "test-nodejs-dima";
+// const filePath = "./tmp/8777a8bb-b5b8-410b-833f-58b8542e8c79.jpg";
+// const destFileName = "8777a8bb-b5b8-410b-833f-58b8542e8c79.jpg";
+
+const downloadFilePath = "8777a8bb-b5b8-410b-833f-58b8542e8c79.jpg";
+const downloadFileName = "./tmp/new-file.jpg";
+
 async function launchDemo() {
-  await storage.bucket(bucketName).upload(filePath, {
-    destination: destFileName,
+  ///Downloading
+  await storage.bucket(bucketName).file(downloadFilePath).download({
+    destination: downloadFileName,
   });
-  console.log(`${filePath} uploaded to ${bucketName}`);
+
+  console.log(
+    `gs://${bucketName}/${downloadFilePath} downloaded to ${downloadFileName}.`
+  );
+
+  /// Uploading
+  //   await storage.bucket(bucketName).upload(filePath, {
+  //     destination: destFileName,
+  //   });
+  //   console.log(`${filePath} uploaded to ${bucketName}`);
+
+  // GOOGLE_APPLICATION_CREDENTIALS=/Users/vladimirdushatsky/Desktop/MyDIsk/goit/nodeJs/nodeJsProject/gcp.json node google-storage-demo.js
 }
 
 launchDemo().catch(console.error);
