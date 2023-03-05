b5fe1a241a8eb2e0920fac85ec473afaf6b7f3d7
Dima Savkov <dimsaw85@gmail.com>
2023-03-05 14:39:18 +0200

    finish

--- __tests__/postServerTest.js
+++ __tests__/postServerTest.js
@@ -1,23 +1,27 @@
 const { getPostsById } = require('../src/services/postsService');
 const { WrongsParametersError } = require('../src/helpers/errors');
 const { Post } = require('../src/db/postModel');
-const { JsonWebTokenError } = require('jsonwebtoken');
-const { User } = require('../src/db/userModel');
+
 
 describe("Posts Service getPostsById test ", () => {
-    it("should retern post data by providen id ", async (done) => {
+    it("should retern post data by providen id ", async () => {
+        const mPostId = '1';
+       const mUserId = '2';
+
         const post = {
             _id: mPostId,
             topic: 'topic',
             userId: mUserId,
             text: 'text'
        }
-       const mPostId = '1';
-       const mUserId = '2';
        
-       jest.spyOn(User, 'findOne').mockImplementationOnce(() => post)
-       const result = await getPostsById(mPostId, mUserId)
-        done();
+       
+       jest.spyOn(Post, 'findOne').mockImplementationOnce( async () => post)
+        const result = await getPostsById(mPostId, mUserId);
+        expect(result._id).toEqual(mPostId);
+        expect(result.topic).toBeDefined();
+        expect(result.text).toBeDefined();
+        
      });
  
 });
